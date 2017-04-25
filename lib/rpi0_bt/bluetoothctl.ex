bb8b69010c9d84e72b05b50d7f0b893eb01b8f70
Klaus Alfert <klaus.alfert@gmail.com>
2017-04-25 23:31:24 +0200

    Logging of commands and bluetoothctl output

--- lib/rpi0_bt/bluetoothctl.ex
+++ lib/rpi0_bt/bluetoothctl.ex
@@ -38,30 +38,74 @@ defmodule Bluetooth.Ctl do
     GenServer.call(__MODULE__, {:cmd, cmd})
   end
 
-  defstruct [port: nil, verbose: true]
+  defstruct [port: nil, verbose: true, recording: true, log: nil]
 
   def init([]) do
     path = System.find_executable("bluetoothctl")
     port = Port.open({:spawn_executable, path},
-      [:binary, :use_stdio, :stderr_to_stdout, :stream])
-    {:ok, %__MODULE__{port: port}}
+      [:binary, :use_stdio, :stderr_to_stdout, :stream]) # {:line, 2048}])
+    {:ok, %__MODULE__{port: port, log: "/root/bluetoothd.log"}}
   end
 
-  def handle_info({port, {:data, data}}, %__MODULE__{verbose: verbose} = state) do
-    if verbose, do: IO.puts("BluetoothCtl: #{data}")
+  def handle_info({port, {:data, data_string}}, %__MODULE__{verbose: verbose} = state) when
+          is_binary(data_string) do
+    if verbose, do: IO.puts("BluetoothCtl: #{inspect data_string}")
+    data_string
+    |> log(state.log)
+    |> strip_ansi_sequences()
+    |> String.splitter("\n")
+    |> Enum.map(&parse/1)
+    |> Enum.each(fn p -> IO.puts("Parsed: #{inspect p}") end)
     {:noreply, state}
   end
   def handle_info({port, :closed}, %__MODULE__{verbose: verbose} = state) do
     if verbose, do: IO.puts("BluetoothCtl: closed")
     {:stop, :normal, state}
   end
+  def handle_info({port, {:data, data_string}}, %__MODULE__{verbose: verbose} = state) do
+    if verbose, do: IO.puts("BluetoothCtl: #{inspect data_string}")
+    {:noreply, state}
+  end
 
   def handle_call({:cmd, data}, _from, %__MODULE__{port: port} = state) do
+    log(data, state.log)
     Port.command(port, data)
     |> reply(state)
   end
 
   defp reply(ret_value, state) do
     {:reply, ret_value, state}
+  end
+
+  @doc """
+  Write the message `msg` to the file with name `filename` and
+  returns `msg`.
+  """
+  def log(msg, filename) do
+    :ok = File.write(filename, msg, [:append, :sync, :utf8])
+    msg
+  end
+
+  @doc """
+  Removes the color code ANSI sequences and the `CR` character (`\r`).
+  """
+  def strip_ansi_sequences(s) do
+    color_pattern = "\\e\\[[0-9;]*m"
+    clear_line_pattern = "(\\r)|(\\e\\[K)"
+    {:ok, regexp} = Regex.compile("(#{color_pattern})|(#{clear_line_pattern})")
+    #String.replace(s, regexp, "")
+    Regex.replace(regexp, s, "")
+  end
+
+  # def parse("[bluetooth]# \r" <> event), do: parse_event(event)
+  def parse("[NEW] " <> new_event), do: {:new, parse_device(new_event)}
+  def parse("[CHG] " <> chg_event), do: {:change, parse_device(chg_event)}
+  def parse(output), do: {output}
+
+  def parse_device(<<"Device ", uuid :: binary-size(17), " ", rest::binary>>) do
+    {:device, uuid, rest}
+  end
+  def parse_device(<<"Controller ", uuid :: binary-size(17), " ", rest::binary>>) do
+    {:controller, uuid, rest}
   end
 end
