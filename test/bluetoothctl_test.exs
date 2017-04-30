8a44f3d1bd9fc4216f5d9ab80fa2c642731a0ee9
Klaus Alfert <klaus.alfert@gmail.com>
2017-04-30 19:47:56 +0200

    Testing of bluetoothctl parsing and interaction with BLE abstraction

--- test/bluetoothctl_test.exs
+++ test/bluetoothctl_test.exs
@@ -2,12 +2,36 @@ defmodule Bluetooth.Test.Ctl do
 
   use ExUnit.Case
 
-  test "parsing first testscript" do
-    controller = Bluetooth.Ctl.start(fn -> IO.puts "Hey Hey Hey" end)
-    script = "test/bluetoothd_script_1.txt"
+  alias Bluetooth.Ctl
+  alias Bluetooth.GenBle
+
+  setup do
+    {:ok, controller} = Bluetooth.Ctl.start(fn -> IO.puts "Hey Hey Hey" end)
+    on_exit(fn ->
+      ref = Process.monitor(controller)
+      Process.exit(controller, :kill)
+      receive do
+        {:DOWN, ^ref, _, ^controller, _} -> :ok
+      end
+    end)
+    {:ok, %{controller: controller}}
+  end
+
+  def script_as_list(filename) do
+    filename
     |> File.read!()
     |> String.splitter("\n")
     |> Enum.map(&Bluetooth.Ctl.strip_ansi_sequences/1)
+  end
+
+  def parse_and_filter_commands(events) do
+    events
+    |> Enum.map(&Ctl.parse/1)
+    |> Enum.filter(&Ctl.is_parsed_event?/1)
+  end
+
+  test "parsing first testscript" do
+    script = script_as_list "test/bluetoothd_script_1.txt"
 
     assert is_list(script)
 
@@ -35,4 +59,37 @@ defmodule Bluetooth.Test.Ctl do
         fn [p, _] -> String.ends_with?(p, "[bluetooth]") end)
   end
 
+  test "parse some device states from script 1", %{controller: controller} do
+    script = script_as_list "test/bluetoothd_script_1.txt"
+
+    [bluez_start | rest] = script
+
+    tree = Ctl.parse(bluez_start)
+    assert {:new, {:controller, "B8:27:EB:CC:05:DA", "BlueZ 5.43 [default]"}} == tree
+
+    power_on = rest
+    |> parse_and_filter_commands()
+    |> List.first
+    assert {:change, {:controller, "B8:27:EB:CC:05:DA", powered: true}} == power_on
+  end
+
+  test "manage device state from script 1",  %{controller: controller} do
+    script = "test/bluetoothd_script_1.txt"
+    |> script_as_list()
+    |> parse_and_filter_commands()
+    {:ok, ble} = GenBle.start_link(controller)
+
+    assert [] == GenBle.controllers(ble)
+
+    bluez_start = Enum.at(script, 0)
+    power_on = Enum.at(script, 1)
+    {:new, {:controller, c_id, name}} = bluez_start
+
+    send(ble, bluez_start)
+    assert [%GenBle.Controller{id: c_id, name: name, powered: false}] = GenBle.controllers(ble)
+
+    send(ble, power_on)
+    assert [%GenBle.Controller{id: c_id, name: name, powered: true}] = GenBle.controllers(ble)
+
+  end
 end
