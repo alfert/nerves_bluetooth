f754ec7f2b9609a1bce62e1345a41c813f9137da
Klaus Alfert <klaus.alfert@gmail.com>
2017-05-05 20:17:44 +0200

    Take a first drop from js hci sources

--- src/hci_module.c
+++ src/hci_module.c
@@ -10,10 +10,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdarg.h>
-
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #include "hci_module.h"
 
@@ -34,6 +38,103 @@ void flog(const char *fmt, ...)  {
 
 #endif
 
+#define BTPROTO_L2CAP 0
+#define BTPROTO_HCI   1
+
+#define SOL_HCI       0
+#define HCI_FILTER    2
+
+#define HCIGETDEVLIST _IOR('H', 210, int)
+#define HCIGETDEVINFO _IOR('H', 211, int)
+
+#define HCI_CHANNEL_RAW     0
+#define HCI_CHANNEL_USER    1
+#define HCI_CHANNEL_CONTROL 3
+
+#define HCI_DEV_NONE  0xffff
+
+#define HCI_MAX_DEV 16
+
+#define ATT_CID 4
+
+
+struct sockaddr_hci {
+  sa_family_t     hci_family;
+  unsigned short  hci_dev;
+  unsigned short  hci_channel;
+};
+
+struct hci_dev_req {
+  uint16_t dev_id;
+  uint32_t dev_opt;
+};
+
+struct hci_dev_list_req {
+  uint16_t dev_num;
+  struct hci_dev_req dev_req[0];
+};
+
+typedef struct {
+  uint8_t b[6];
+} __attribute__((packed)) bdaddr_t;
+
+struct hci_dev_info {
+  uint16_t dev_id;
+  char     name[8];
+
+  bdaddr_t bdaddr;
+
+  uint32_t flags;
+  uint8_t  type;
+
+  uint8_t  features[8];
+
+  uint32_t pkt_type;
+  uint32_t link_policy;
+  uint32_t link_mode;
+
+  uint16_t acl_mtu;
+  uint16_t acl_pkts;
+  uint16_t sco_mtu;
+  uint16_t sco_pkts;
+
+  // hci_dev_stats
+  uint32_t err_rx;
+  uint32_t err_tx;
+  uint32_t cmd_tx;
+  uint32_t evt_rx;
+  uint32_t acl_tx;
+  uint32_t acl_rx;
+  uint32_t sco_tx;
+  uint32_t sco_rx;
+  uint32_t byte_rx;
+  uint32_t byte_tx;
+};
+
+struct sockaddr_l2 {
+  sa_family_t    l2_family;
+  unsigned short l2_psm;
+  bdaddr_t       l2_bdaddr;
+  unsigned short l2_cid;
+  uint8_t        l2_bdaddr_type;
+};
+
+/* =========================================*/
+
+int _mode;
+int _socket;
+int _devId;
+
+/* =========================================*/
+
+
+int init_hci() {
+  _socket = socket(AF_BLUETOOTH, SOCK_RAW | SOCK_CLOEXEC, BTPROTO_HCI);
+
+}
+
+
+/** Simple test functions */
 int foo(int x) {
   return x+1;
 }
