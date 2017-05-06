1d869e756f9a835eb4898b8b962bdce1b3c63d4a
Klaus Alfert <klaus.alfert@googlemail.com>
2017-05-06 12:47:09 +0200

    test hci_init

--- test/hci_port_test.exs
+++ test/hci_port_test.exs
@@ -5,10 +5,17 @@ defmodule Bluetooth.Test.HCIPort do
 
   test "call foo" do
     {:ok, hci} = HCI.start_link()
+    assert hci == GenServer.whereis(HCI)
     x = 5
     y = HCI.foo(x)
     assert y == x + 1
     HCI.stop()
   end
+
+  test "call hci_init" do
+    {:ok, hci} = HCI.start_link()
+    assert :ok = HCI.hci_init()
+    HCI.stop()
+  end 
 
 end
